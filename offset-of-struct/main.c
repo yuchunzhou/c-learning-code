787e6fad6212badd9a2961e4f827e0c6971c4262
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-07-26 16:20:38 +0800

    add offset-of-struct

---
+++ offset-of-struct/main.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+#include "log.h"
+
+int main(int argc, char *argv[]) {
+    struct Foo {
+        char a;
+        short b;
+        long int c;
+    };
+    log_info("offset of b: %ld", offsetof(struct Foo, b));
+    log_info("offset of c: %ld", offsetof(struct Foo, c));
+
+    struct Foo foo = {0};
+    *((char *)&foo + offsetof(struct Foo, c)) = 2;
+    log_info("c: %ld", foo.c);
+
+    return 0;
+}
