c4d2f4d97ad35cc10282ebd79dd1fee6a9f66add
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-08-15 16:50:16 +0800

    update

--- random-number/main.c
+++ random-number/main.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+
+#include "log.h"
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
     for (int i = 0; i < 10; i++) {
         int random_num = rand();
-        printf("new random number is generated: %d\n", random_num);
+        log_info("new random number is generated: %d", random_num);
         sleep(1);
     }
 
