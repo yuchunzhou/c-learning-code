5b81f9941096e0d0daf4b0f294dfad0577278fc5
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-08-26 16:13:58 +0800

    Fix return value check

--- cpu-affinity/main.c
+++ cpu-affinity/main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
     memset(&thread, 0, sizeof(pthread_t));
 
     int ret = pthread_create(&thread, NULL, thread_func, NULL);
-    if (ret < 0) {
+    if (ret != 0) {
         log_error("failed to create thread: %s", strerror(errno));
         exit(1);
     }
@@ -48,12 +48,13 @@ int main(int argc, char *argv[]) {
     CPU_SET(3, &cpu_set);
 
     ret = pthread_setaffinity_np(thread, 1, &cpu_set);
-    if (ret < 0) {
+    if (ret != 0) {
         log_error("failed to set cpu affinity for thread: %s", strerror(errno));
         exit(1);
     }
     log_info(">>> cpu affinity has been setted <<<");
 
     sleep(100);
+
     return 0;
 }
